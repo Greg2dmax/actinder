980f2cf841fd8a15ac88d89352c9c0cdc913ab93
Greg2dmax <sportzballa2000@gmail.com>
2020-06-13 17:41:41 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,106 @@
+export default [
+  {
+    pics: [
+      "https://i.kym-cdn.com/photos/images/newsfeed/001/847/286/671.jpg"
+    ],
+    name: "Ankha",
+    age: 32,
+    distance: "40 Nookmiles away",
+    text:
+      "Hi, My name is Ankha and I loves cherries and is allergic to apples. My favorite hobby is observing nature."
+  },
+  {
+    pics: [
+      "https://pbs.twimg.com/profile_images/1250253142681358337/v9nE4RPL_400x400.jpg"
+    ],
+    name: "Marshal",
+    age: 18,
+    distance: "10 Nookmiles away",
+    text:
+      "I keep calling the plumber round, and he keeps fixing stuff. Where am I going wrong?"
+  },
+  {
+    pics: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRI4yPvp3EHb6OmOq8pm5tFHqfcS2Yzuu8QB-SO0f32eX5Iq77J&usqp=CAU"
+    ],
+    name: "Judy",
+    age: 19,
+    distance: "300 Nookmiles away",
+    text: "Aspiring MILF"
+  },
+  {
+    pics: [
+      "https://animalcrossingworld.com/wp-content/uploads/2019/09/animal-crossing-new-horizons-villager-goldie.jpg"
+    ],
+    name: "Goldie",
+    age: 29,
+    distance: "30 Nookmiles away",
+    text: "A little known fact is that I cover about 40% of Africa."
+  },
+  {
+    pics: [
+      "https://pbs.twimg.com/media/EUUMnYpWsAEqU2A.jpg",
+      "https://vignette.wikia.nocookie.net/animalcrossing/images/1/14/NH-Bob_poster.png/revision/latest/scale-to-width-down/340?cb=20200410181927"
+    ],
+    name: "Bob",
+    age: 22,
+    distance: "20 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://pbs.twimg.com/media/EU64zSDX0AAXYpC.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvRPLwoMWQAmSiR-gnP6IZAiG0NSvnXzFwZbyMFLAq7bZKQpFz&usqp=CAU"
+    ],
+    name: "Roald",
+    age: 22,
+    distance: "25 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://pbs.twimg.com/media/EUiJImkU8AAy775.jpg",
+      "https://dodo.ac/np/images/3/30/BeauPicACNH.png"
+    ],
+    name: "Beau",
+    age: 22,
+    distance: "60 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://www.belltreeforums.com/attachments/13ccf061-678e-408a-8f4e-7e221c9c409a-png.252808/",
+      "https://66.media.tumblr.com/35aa400e59a41b3aad491b83d3508e45/87f9bead3bbd889f-dc/s400x600/2a685a5ca8ff7e2f2399c988ea3c1e7bc00efe56.png"
+    ],
+    name: "Diana",
+    age: 22,
+    distance: "40 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://pbs.twimg.com/media/EVwCQHPVcAA-RXf.jpg",
+      "https://66.media.tumblr.com/b65d81a5fdc8fa43dd68e5d2812933cc/41608d11806c8301-81/s500x750/27a637c43aa696ca5076a354835af90956f004da.jpg"
+    ],
+    name: "Ketchup",
+    age: 22,
+    distance: "20 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  },
+  {
+    pics: [
+      "https://66.media.tumblr.com/c139b58089f076a053b408e9e4cc119e/eda57be911b01108-c3/s500x750/6fcb94e8f10fa860e7cc22e441b14759cabc47c1.jpg",
+      "https://66.media.tumblr.com/3206ba38455dfcba8a38717ece16f1cc/3893d1edf36466cc-77/s500x750/2232519a9a68487040fa3504f4d7144db9eb397b.png"
+    ],
+    name: "Fauna",
+    age: 22,
+    distance: "20 Nookmiles away",
+    text:
+      "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+  }
+];
