4d8a79c9b22db8ae484145ef24d26fea95993ae0
Greg2dmax <sportzballa2000@gmail.com>
2020-06-13 18:24:53 -0400

    updated for deployment

---
+++ build/precache-manifest.12f6695ea63e186b11275af1d8d5ed93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9e0f709936e201a06a149991fd1147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63b0777c3ff0ef5e37f",
+    "url": "/static/css/main.a2787670.chunk.css"
+  },
+  {
+    "revision": "d0d47ebbe7cb47478f26",
+    "url": "/static/js/2.ae68def5.chunk.js"
+  },
+  {
+    "revision": "49b5983f09fdd760fd58bf56c788574d",
+    "url": "/static/js/2.ae68def5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63b0777c3ff0ef5e37f",
+    "url": "/static/js/main.151c70cc.chunk.js"
+  },
+  {
+    "revision": "c3526385f91fd34a852f",
+    "url": "/static/js/runtime-main.fd1318aa.js"
+  }
+]);
